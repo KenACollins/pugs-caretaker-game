9cfb1e0422e77a9bb0893c8183485480221a3eda
Ken A Collins <Ken@KenACollins.com>
2019-12-08 13:34:58 -0500

    Testing pugs route handler in attempt to get response.

--- routes/pugRoutes.js
+++ routes/pugRoutes.js
@@ -1,11 +1,12 @@
 
 module.exports = app => {
     app.get('/proxy/random', (req, res, next) => {
-        //res.send('Thanks!');
+        console.log('Inside pugRoutes...');
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        //res.send();
         console.log('res', res);
-        next();
+        //next();
+        res.send('Thanks!');
+        //res.send();
     });
 }
