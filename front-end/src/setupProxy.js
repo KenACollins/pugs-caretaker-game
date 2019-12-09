f88f06c459b39d3168a94a0b8a5f75fa061c1f6f
Ken A Collins <Ken@KenACollins.com>
2019-12-08 21:16:59 -0500

    File not needed.

--- front-end/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-// Applicable to create-react-app (CRA) version 2 or higher.
-// This file does not need to be imported anywhere.  CRA looks for a file by this name and loads it.
-const proxy = require('http-proxy-middleware');
- 
-module.exports = function(app) {
-    // For the relative URIs specified in the array below, redirect to the same target.
-    app.use(proxy(['/random'], { target: 'http://pugme.herokuapp.com' }));
-};
