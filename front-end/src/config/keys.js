ef7fffd3a765cfe5791f8bdfba3d429daea2d85f
Ken A Collins <Ken@KenACollins.com>
2019-12-09 12:30:33 -0500

    Testing environment - Heroku should report 'production' when running app there.

--- front-end/src/config/keys.js
+++ front-end/src/config/keys.js
@@ -1,5 +1,5 @@
 // keys.js - Figure out which set of credentials to return based on environment (dev or prod).
-// console.log('****************** FRONT-END RUNNING IN: ' + process.env.NODE_ENV);
+console.log('****************** FRONT-END RUNNING IN: ' + process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
     // We are in production, return prod set of keys.
     module.exports = require('./prod');
